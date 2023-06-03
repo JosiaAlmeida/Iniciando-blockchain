b457c92dcba9965f63e43a964f6c1fafaf31a863
Josia Almeida <71391114+JosiaAlmeida@users.noreply.github.com>
2023-06-03 18:06:50 +0100

    feat: criando metodo para hash

--- blockchain.py
+++ blockchain.py
@@ -26,4 +26,10 @@ class Blockchain:
     def proof_of_work(self, previous_proof):
         new_proof = 1
         check_proof = False
-        
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof +=1
+        return new_proof
