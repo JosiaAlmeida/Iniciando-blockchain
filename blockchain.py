4fd34c1de886d33d2ebb6a012ec77d27f1307bda
Josia Almeida <71391114+JosiaAlmeida@users.noreply.github.com>
2023-06-03 20:25:11 +0100

    feat: criando mineração

--- blockchain.py
+++ blockchain.py
@@ -53,3 +53,24 @@ class Blockchain:
             previous_block = block
             block_index +=1
         return True
+    
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+blockchain = Blockchain()
+
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof) 
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Parabéns você minerou um bloco!', 'block': block}
+    return jsonify(response), 200
+
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain, 'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+app.run(host='0.0.0.0', port=5001)
